3474c8731f1de46f5cf5727802d85c3443bf2102
LNayana <lokhandenayana22@gmail.com>
2018-02-25 13:09:13 +0530

    added webpack dev server to use development server instead of live-server

--- src/app.js
+++ src/app.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import DemoApp from './components/DemoApp';
 
-ReactDOM.render( template , document.getElementById('DemoContainer'));
+ReactDOM.render( <DemoApp/> , document.getElementById('DemoContainer'));
